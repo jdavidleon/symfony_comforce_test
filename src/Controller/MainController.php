024b75c7fafae484cc4a51f1435db848bf480add
Juan Leon <jlp25@hotmail.com>
2018-09-02 23:14:26 -0500

    Add Register and Login Users

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,17 +3,62 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/{user}", name="homepage")
      */
-    public function index()
-    {
+    public function index(Request $request, $user=null, AuthenticationUtils $authenticationUtils)
+    {	
+    	 // get the login error if there is one
+	    $error = $authenticationUtils->getLastAuthenticationError();
+
+	    // last username entered by the user
+	    $lastUsername = $authenticationUtils->getLastUsername();
+	    
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'MainController',
+            'user' => $user,
+            'last_username' => $lastUsername,
+	        'error'         => $error,
+        ]);
+    }
+
+
+    /**
+     * @Route("/user/register", name="register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {   
+        $user = new User();
+        // Form Constructor
+        $form = $this->createForm(UserType::class, $user);
+
+        // Recive info
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            // 4) save the User!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('homepage', ['user' => 'created']);
+        }
+
+        return $this->render('home/register.html.twig', [
+            'form'=>$form->createView()
         ]);
     }
 }
---
+++ src/Form/UserType.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('username', TextType::class,array('label' => 'Usuario' ))
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Contraseña'),
+                'second_options' => array('label' => 'Repetir Contraseña'),
+            ))
+            ->add('save', SubmitType::class, array('label' => 'Registrarse' ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => User::class,
+        ));
+    }
+}
